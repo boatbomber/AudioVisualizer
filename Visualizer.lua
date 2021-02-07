e495dfb3ee5730ebdaa573f14d7a913cf8e3248e
boatbomber <40185666+boatbomber@users.noreply.github.com>
2021-02-07 10:06:26 -0800

    Create Visualizer.lua

---
+++ Visualizer.lua
@@ -0,0 +1,161 @@
+local RunService = game:GetService("RunService")
+
+local module = {}
+
+function module.new(Frame, BarCount)
+
+	-- Determine settings
+	local BAR_COUNT = math.clamp(typeof(BarCount) == "number" and BarCount or 41, 11,601)
+	local SAMPLE_HZ = math.clamp(BAR_COUNT/2,10,500)
+
+	if BAR_COUNT%2==0 then
+		BAR_COUNT += 1
+	end
+
+	local BAR_SIZE = 1/BAR_COUNT
+	local BUFFER_COUNT = math.ceil(BAR_COUNT/2)
+	local BAR_TO_BUFFER = table.create(BAR_COUNT)
+	local BAR_TO_MULTIPLIER = table.create(BAR_COUNT)
+
+	for i=1, BAR_COUNT do
+		BAR_TO_BUFFER[i] = math.abs(BUFFER_COUNT- (i+ (i<=BUFFER_COUNT and -1 or 1) ))
+		BAR_TO_MULTIPLIER[i] = 1-(math.clamp(math.abs(BUFFER_COUNT- (i+ (i<=BUFFER_COUNT and -1 or 1) ))/BUFFER_COUNT, 0.01,1)^2)
+	end
+	
+	local UPDATE_WAIT = 1/SAMPLE_HZ
+	
+	-- Setup visualizer
+	
+	local Visualizer = {
+		Frame = Frame;
+		VolumeBuffer = table.create(BUFFER_COUNT+1);
+		Bars = {};
+		BufferConnection = nil;
+		PlayingConnection = nil;
+		Sound = nil;
+	}
+	
+	-- Create the bar guis
+	for i=1, BAR_COUNT do
+		local Bar = Instance.new("Frame")
+		Bar.BorderSizePixel = 0
+		Bar.BackgroundColor3 = Color3.new(0.98,0.98,0.99)
+		Bar.AnchorPoint = Vector2.new(0,0.5)
+		Bar.Size = UDim2.new(BAR_SIZE,0,0.02,0)
+		Bar.Position = UDim2.new(BAR_SIZE* (i-1), 0,0.5,0)
+		
+		Visualizer.Bars[i] = Bar
+		
+		Bar.Parent = Frame
+	end
+	
+	local function HandlePlaying()
+		if Visualizer.BufferConnection then
+			Visualizer.BufferConnection:Disconnect()
+			Visualizer.BufferConnection = nil
+		end
+	
+		local Sound = Visualizer.Sound
+		if not Sound then return end
+
+		if Sound.Playing then
+			local LastStored = os.clock()
+			Visualizer.BufferConnection = RunService.Heartbeat:Connect(function()
+
+				Visualizer.VolumeBuffer[1] = Sound.PlaybackLoudness
+				Visualizer.Bars[BUFFER_COUNT]:TweenSize(
+					UDim2.new(
+						BAR_SIZE,0,
+						math.clamp((Sound.PlaybackLoudness/400), 0.02,1),0
+					),
+					Enum.EasingDirection.Out, Enum.EasingStyle.Linear, 0.014, true
+				)
+
+				if os.clock()-LastStored > UPDATE_WAIT then
+					LastStored = os.clock()
+
+					table.insert(Visualizer.VolumeBuffer,1,Sound.PlaybackLoudness)
+					local Length = #Visualizer.VolumeBuffer
+					if Length > BUFFER_COUNT then
+						Visualizer.VolumeBuffer[#Visualizer.VolumeBuffer] = nil
+					end
+
+					for i,Bar in ipairs(Visualizer.Bars) do
+						Bar:TweenSize(
+							UDim2.new(
+								BAR_SIZE,0,
+								math.clamp(
+									((Visualizer.VolumeBuffer[BAR_TO_BUFFER[i]] or 0)/400)*BAR_TO_MULTIPLIER[i],
+									0.02, 1),0
+							),
+							Enum.EasingDirection.Out, Enum.EasingStyle.Linear, UPDATE_WAIT, true
+						)
+					end
+				end
+
+			end)
+		else
+			table.clear(Visualizer.VolumeBuffer)
+			for i,Bar in ipairs(Visualizer.Bars) do
+				Bar:TweenSize(
+					UDim2.new(BAR_SIZE,0,0.02,0),
+					Enum.EasingDirection.Out, Enum.EasingStyle.Linear, UPDATE_WAIT, true
+				)
+			end
+		end
+	end
+	
+	function Visualizer:LinkToSound(Sound)
+		
+		Visualizer:UnlinkFromSound()
+	
+		table.clear(Visualizer.VolumeBuffer)
+		
+		Visualizer.Sound = Sound
+		
+		HandlePlaying()
+		Visualizer.PlayingConnection = Sound:GetPropertyChangedSignal("Playing"):Connect(HandlePlaying)
+	end
+	
+	function Visualizer:UnlinkFromSound()
+
+		if Visualizer.PlayingConnection then
+			Visualizer.PlayingConnection:Disconnect()
+			Visualizer.PlayingConnection = nil
+		end
+		if Visualizer.BufferConnection then
+			Visualizer.BufferConnection:Disconnect()
+			Visualizer.BufferConnection = nil
+		end
+
+		Visualizer.Sound = nil
+		
+		for i,Bar in ipairs(Visualizer.Bars) do
+			Bar:TweenSize(
+				UDim2.new(BAR_SIZE,0,0.02,0),
+				Enum.EasingDirection.Out, Enum.EasingStyle.Linear, UPDATE_WAIT, true
+			)
+		end
+
+	end
+	
+	function Visualizer:Destroy()
+		if Visualizer.PlayingConnection then
+			Visualizer.PlayingConnection:Disconnect()
+			Visualizer.PlayingConnection = nil
+		end
+		if Visualizer.BufferConnection then
+			Visualizer.BufferConnection:Disconnect()
+			Visualizer.BufferConnection = nil
+		end
+		for i,Bar in ipairs(Visualizer.Bars) do
+			Bar:Destroy()
+		end
+		table.clear(Visualizer.VolumeBuffer)
+	end
+	
+	
+	return Visualizer
+end
+
+return module
